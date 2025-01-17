b6ba8169c1c2e88fd4aafd22370c764ce526867e
tomjosephthomas <91357052+tomjosephthomas@users.noreply.github.com>
2025-01-17 13:25:43 +0800

    Update PX_Gather_Logs.sh

--- PX_Gather_Logs.sh
+++ PX_Gather_Logs.sh
@@ -362,7 +362,7 @@ done
 for i in "${!pxctl_commands[@]}"; do
   cmd="${pxctl_commands[$i]}"
   echo "cmd: $cmd "
-#  output_file="$output_dir/${pxctl_output_files[$i]}"
+  output_file="$output_dir/${pxctl_output_files[$i]}"
   #echo "Executing: pxctl $cmd"
 #  $cli -n $namespace $pxcmd $cmd > "$output_file" 2>&1 || true
 #  echo "Output saved to: $output_file"
