46836dcf0b08ed82468b195bef4832ee5344c982
tomjosephthomas <91357052+tomjosephthomas@users.noreply.github.com>
2025-01-17 08:52:22 +0800

    Update PX_Gather_Logs.sh

--- PX_Gather_Logs.sh
+++ PX_Gather_Logs.sh
@@ -365,7 +365,7 @@ for i in "${!pxctl_commands[@]}"; do
   #echo "Executing: pxctl $cmd"
   $cli -n $namespace $pxcmd $cmd > "$output_file" 2>&1
   echo "Output saved to: $output_file"
-  echo $?
+  echo "Return Code: $?"
 done
 
 
